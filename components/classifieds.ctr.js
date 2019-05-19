d41bc4d8f66e9cf715941f346560e56f57a633d6
David Rhodes <david@davidrhodes.work>
2019-05-18 21:32:21 -0400

    feat(new_classified): adds UI for new classified

--- components/classifieds.ctr.js
+++ components/classifieds.ctr.js
@@ -3,7 +3,7 @@
 
   angular
     .module('moviePosterDeals')
-    .controller('classifiedsCtrl', ($scope, classifiedsFactory, $mdSidenav) => {
+    .controller('classifiedsCtrl', ($scope, classifiedsFactory, $mdSidenav, $mdToast) => {
       classifiedsFactory.getClassifieds()
       .then(foundData => {
         $scope.classifieds = foundData.data
@@ -16,6 +16,29 @@
       $scope.closeSidebar = () => {
         $mdSidenav('left').close();
       };
-    });
 
+      $scope.saveClassified = classified => {
+        if (classified.movie_title && classified.price && classified.description && classified.image){
+          // TODO: add user auth, current user (remove this dummy data for UI step)
+          const dummyCurrentUser = {
+            name: 'David Rhodes',
+            phone: '646-477-6360',
+            email: 'rhodesdav@gmail.com'
+          };
+          classified.contact = dummyCurrentUser;
+
+          $scope.classifieds.push(classified);
+          $scope.classified = {};
+          $scope.closeSidebar();
+
+          $mdToast.show(
+            $mdToast.simple()
+            .content("Classified Saved!")
+            .position('top right')
+            .hideDelay(3000)
+          );
+        }
+      };
+
+    });
 })();
